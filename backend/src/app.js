7316d2433eaadc400510e62d572c89b3c3623d09
Josue R S Jacinto <jr.waydev@gmail.com>
2023-03-19 14:01:41 -0300

    backend deletada

--- backend/src/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', (req, res) => res.sendStatus(200).send('Olá'));
-
-module.exports = app;
--- backend/src/server.js
+++
@@ -1,4 +0,0 @@
-const app = require('./app');
-const port = 3050;
-
-app.listen(port, () => console.log('Server running on port 3050'));
